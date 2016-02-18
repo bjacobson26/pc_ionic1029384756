de3866ae9e1a9dd69728011722238ef447139f8b
Bo Jacobson <bo.jacobson@avant.com>
2016-02-18 00:19:22 -0800

    comment like buttons

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -46,7 +46,81 @@ app.controller('HomeCtrl', function($scope){
 
 });
 
+
 app.controller('CongressCtrl', function($scope, Congress) {
+
+  var commentScore = 'undecided';
+
+  $scope.likeComment = function(comment){
+    if(commentScore == 'undecided'){
+      commentScore = 'upvote';
+      comment.likes++;
+      document.getElementById('like_comment').className += ' blue';
+    }
+    else if(commentScore == 'downvote'){
+      commentScore = 'upvote';
+      comment.likes++;
+      comment.dislikes--;
+      document.getElementById('like_comment').className += ' blue';
+      document.getElementById('unlike_comment').className = 'button';
+    }
+    else if(commentScore == 'upvote'){
+      commentScore = "undecided";
+      comment.likes--;
+      document.getElementById('like_comment').className = 'button';
+    }
+  }
+
+  $scope.unlikeComment = function(comment){
+    if(commentScore == 'undecided'){
+      commentScore = 'downvote';
+      comment.dislikes++;
+      document.getElementById('unlike_comment').className += ' red';
+    }
+    else if(commentScore == 'downvote'){
+      comment.dislikes--;
+      commentScore = 'undecided';
+      document.getElementById('unlike_comment').className = 'button';
+    }
+    else if(commentScore == 'upvote'){
+      commentScore = 'downvote';
+      comment.likes--;
+      comment.dislikes++;
+      document.getElementById('like_comment').className = 'button';
+      document.getElementById('unlike_comment').className += ' red';
+    }
+  }
+  
+  $scope.viewComments = false;
+  $scope.comments = [
+    { 
+      'user': 'XxPoopsBaldEaglezxX', 
+      'text': "We worship an awesome God in the Blue States, and we don't like federal agents poking around in our libraries in the Red States. Now, I don't believe that Senator McCain doesn't care what's going on in the lives of Americans. You're on your own. You can't truly stand up for Georgia when you've strained our oldest alliances. The fear and anger that it provoked was understandable, but in some cases, it led us to act contrary to our ideals. All these things must be done in partnership.",
+      'likes': 3640,
+      'dislikes': 3253
+    },
+    {
+      'user': 'LiberalCat', 
+      'text': "The hope of a skinny kid with a funny name who believes that America has a place for him, too. Hope in the face of difficulty. They weren't simply a religious leader's effort to speak out against perceived injustice. What the nay-sayers don't understand is that this election has never been about me. Tomorrow, I will visit Buchenwald, which was part of a network of camps where Jews were enslaved, tortured, shot and gassed to death by the Third Reich.",
+      'likes': 245,
+      'dislikes': 6432
+    },
+
+    { 
+      'user': 'BernieSandersOffical', 
+      'text': "You've got the top 400 Americans owning more wealth than the bottom 150 million Americans. Most folks do not think that is right.",
+      'likes': '98999',
+      'dislikes': 3 
+    }
+  ];
+  $scope.openComments = function(){
+    if($scope.viewComments == false){
+      $scope.viewComments = true;
+    }
+    else{
+      $scope.viewComments = false;
+    }
+  }
 
   $scope.yays = 0;
   $scope.nays = 0;
